d23d7bac9e2d9ef00c488b5d79b49d163107a5ea
Benjamin Lyon <benjaminlyon0@gmail.com>
2023-05-25 00:36:43 +0200

    basic trivia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { shuffle } from "lodash-es";
+import {db} from "../schema";
 
 const API_URL = "https://the-trivia-api.com/api/questions";
+
+const registerWin = async (email: string) => {
+  const res = await fetch("/api/win", {method: "POST", body: JSON.stringify({email})});
+}
 
 export default function Page() {
   const [questions, setQuestions] = useState([]);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     axios.get(API_URL).then((response) => {
@@ -15,38 +22,66 @@ export default function Page() {
     });
   }, []);
 
-  const firstQuestion = questions.length > 0 ? questions[0].question : "";
-  const getCorrectAnswer = questions.length > 0 ? questions[0].correctAnswer : "";
-  const getIncorrectAnswers = questions.length > 0 ? questions[0].incorrectAnswers : [];
+  const firstQuestion = questions?.[0];
+  const questionText = firstQuestion?.question;
+  const correctAnswer = questions.length > 0 ? questions[0].correctAnswer : "";
 
   const handleSelectQuestion = (answer) => {
     setSelectedQuestion(answer);
 
-    const isCorrect = answer === getCorrectAnswer;
+    const isCorrect = answer === correctAnswer;
     setIsAnswerCorrect(isCorrect);
+
+    if (isCorrect) {
+      registerWin(email)
+    }
   };
 
+  const questionButtons = useMemo(
+    () =>
+      firstQuestion
+        ? shuffle([
+            firstQuestion.correctAnswer,
+            ...firstQuestion.incorrectAnswers,
+          ])
+        : [],
+    [firstQuestion]
+  );
+
   return (
-    <div>
-      <h2 className="text-red-500 text-2xl font-bold">First Question</h2>
-      <p>{firstQuestion}</p>
-      <h2>Possible Answers</h2>
-      <button onClick={() => handleSelectQuestion(getCorrectAnswer)}>{getCorrectAnswer}</button>
-      {getIncorrectAnswers.map((answer, index) => (
-        <button key={index} onClick={() => handleSelectQuestion(answer)}>
-          {" "}
-          {answer}
-        </button>
-      ))}
-      {selectedQuestion && (
-        <p>
-          {" "}
-          {isAnswerCorrect
-            ? "Correct answer!"
-            : `Incorrect answer! The correct answer was: ${getCorrectAnswer}`}{" "}
-        </p>
-      )}
-      {/* <pre>{JSON.stringify(questions, null, 2)}</pre> */}
+    <div className="flex">
+      <div className="w-[800px] mx-auto">
+        <input value={email} type="email" className="m-4 border rounded bg-neutral-50" placeholder="Email" onChange={(e) => {console.log(e); setEmail(e.target.value)}} />
+        <h1 className="text-red-500 text-2xl font-bold text-center">
+          Question
+        </h1>
+        <p className="text-center">{questionText}</p>
+        <h2>Possible Answers</h2>
+        <div className="flex flex-row flex-wrap gap-4 justify-center">
+          {questionButtons.map((answer, index) => (
+            <Button key={index} onClick={() => handleSelectQuestion(answer)}>
+              {answer}
+            </Button>
+          ))}
+        </div>
+        {selectedQuestion && (
+          <p>
+            {isAnswerCorrect
+              ? "Correct answer!"
+              : `Incorrect answer! The correct answer was: ${correctAnswer}`}{" "}
+          </p>
+        )}
+        {/* <pre>{JSON.stringify(questions, null, 2)}</pre> */}
+      </div>
     </div>
   );
 }
+
+export const Button = ({ children, onClick }) => (
+  <button
+    onClick={onClick}
+    className="rounded shadow border bg-neutral-100 text-neutral-800 px-4 py-2 hover:bg-neutral-200"
+  >
+    {children}
+  </button>
+);
---
+++ src/app/results/page.tsx
@@ -0,0 +1,20 @@
+import { db } from "../../schema";
+
+
+export default async function Cart({
+  params,
+}: {
+  params: { user: string };
+}): Promise<JSX.Element> {
+    const users = await db.selectFrom('users').selectAll().execute();
+
+  return (
+    <div>
+      {users.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.email}
+        </div>
+      ))}
+    </div>
+  );
+}
