ab826dca37f738ecf019004bc200ab07c7c5f0aa
Benjamin Lyon <benjaminlyon0@gmail.com>
2023-05-24 21:59:16 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ const API_URL = "https://the-trivia-api.com/api/questions";
 
 export default function Page() {
   const [questions, setQuestions] = useState([]);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
 
   useEffect(() => {
     axios.get(API_URL).then((response) => {
@@ -13,16 +15,30 @@ export default function Page() {
     });
   }, []);
 
-  const getFirstQuestion = questions.length > 0 ? questions[0].question : "";
+  const firstQuestion = questions.length > 0 ? questions[0].question : "";
   const getCorrectAnswer = questions.length > 0 ? questions[0].correctAnswer : "";
+  const getIncorrectAnswers = questions.length > 0 ? questions[0].incorrectAnswers : [];
+
+  const handleSelectQuestion = (answer) => {
+    setSelectedQuestion(answer);
+
+    const isCorrect = (answer === getCorrectAnswer);
+    setIsAnswerCorrect(isCorrect);
+  };
 
   return (
     <div>
       <h2>First Question</h2>
-        <p>{getFirstQuestion}</p>
-      <h2>Correct Answer</h2>
-        <p>{getCorrectAnswer}</p>
-        <pre>{JSON.stringify(questions, null, 2)}</pre>
+        <p>{firstQuestion}</p>
+      <h2>Possible Answers</h2>
+        <button onClick={() => handleSelectQuestion(getCorrectAnswer)}>{getCorrectAnswer}</button>
+        {getIncorrectAnswers.map((answer, index) => (
+          <button key={index} onClick={() => handleSelectQuestion(answer)} > {answer}</button>
+        ))}
+        {selectedQuestion && (
+          <p> {isAnswerCorrect ? "Correct answer!" : `Incorrect answer! The correct answer was: ${getCorrectAnswer}`} </p>
+        )}
+        {/* <pre>{JSON.stringify(questions, null, 2)}</pre> */}
     </div>
   );
 }
