cd47f9540f1223c2aa3ef90f09b98e3cab5a7cd3
Benjamin Lyon <benjaminlyon0@gmail.com>
2023-05-29 12:04:51 +0200

    storybook setup

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+
+import '../src/app/globals.css'; // replace with the name of your tailwind css file
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
